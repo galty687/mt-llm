3e4de0a411dc21ebb1b7f0b26757cceb9bfebcfe
Zhijun Gao <galty687@gmail.com>
2024-01-11 08:56:45 +0800

    add 密码保护

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
 import streamlit as st
 from openai import OpenAI
 import requests
+import hmac
+
+# 密码保护
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "请输入智能翻译系统的访问密码：", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 密码错误")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 # 实例化OpenAI对象，并设置API key
 client = OpenAI(api_key=st.secrets['openai_api_key'])
@@ -109,19 +139,89 @@ def review_text_with_openai(text):
 st.title('智能翻译系统')
 
 # Text input
-input_text = st.text_area("Enter text:")
+input_text = st.text_area("输入原文：")
 
 # Language selection for translation
 target_language = st.selectbox(
-    "Select target language:",
+    "选择目标语言：",
     ["zh-CN","en"]
 )
 
 
 
-if st.button('Smart Translate'):
+
+if st.button('谷歌翻译'):
+        if input_text:
+            with st.spinner('谷歌翻译中，请稍候...'):
+
+                cleaned_text = cleaning_text_with_chatgpt(input_text)
+                translated_text_google = translate_text_with_google(cleaned_text, target_language)
+                
+
+
+            st.markdown("**谷歌翻译, Chatgpt润色版本:**")
+            st.write('---')    
+            st.write(translated_text_google)
+            st.write('---')
+        else:
+            st.write("Please enter some text to get translated.")              
+
+
+if st.button('ChatGPT翻译'):
+        if input_text:
+            with st.spinner('ChatGPT翻译中，请稍候...'):
+
+                cleaned_text = cleaning_text_with_chatgpt(input_text)
+                translate_text_with_chatgpt= translate_with_chatgpt(cleaned_text, target_language)
+                
+
+
+            st.markdown("**ChatGPT翻译:**")
+            st.write('---')
+            st.write(translate_text_with_chatgpt)
+            st.write('---')
+        else:
+            st.write("Please enter some text to get translated.")              
+
+
+
+if st.button('🔥谷歌+GPT'):
+        if input_text:
+            with st.spinner('谷歌翻译, Chatgpt润色中，请稍候...'):
+
+                cleaned_text = cleaning_text_with_chatgpt(input_text)
+                translated_text_google = translate_text_with_google(cleaned_text, target_language)
+                edited_text = editing_with_chatgpt(cleaned_text,translated_text_google)
+                
+
+
+            st.markdown("**谷歌翻译, Chatgpt润色:**")    
+            st.write(edited_text)
+            st.write('---')
+        else:
+             st.write("Please enter some text to get translated.")        
+
+
+if st.button('母语审校'):
+        if input_text:
+            with st.spinner('母语审校中，请稍候...'):
+
+                cleaned_text = cleaning_text_with_chatgpt(input_text)
+                translated_text_google = translate_text_with_google(cleaned_text, target_language)
+                edited_text = editing_with_chatgpt(cleaned_text,translated_text_google)
+                reviewed_text = review_with_chatgpt(edited_text)
+                
+
+
+            st.subheader("母语审校:")
+            st.write(reviewed_text)
+            st.write('---')
+        else:
+             st.write("Please enter some text to get translated.")    
+
+if st.button('四种版本同时输出'):
     if input_text:
-        with st.spinner('生成四种版本中... 耗时较长，请稍候。'):
+        with st.spinner('生成四种版本中... 耗时较长，请稍候...'):
                 
             cleaned_text = cleaning_text_with_chatgpt(input_text)
             translated_text_google = translate_text_with_google(cleaned_text, target_language)
@@ -150,17 +250,3 @@ if st.button('Smart Translate'):
         st.success('成功完成')
     else:
         st.write("Please enter some text to get translated.")
-
-if st.button('谷翻模润'):
-    if input_text:
-        with st.spinner('谷歌翻译, Chatgpt润色中，请稍候。'):
-
-            cleaned_text = cleaning_text_with_chatgpt(input_text)
-            translated_text_google = translate_text_with_google(cleaned_text, target_language)
-            edited_text = editing_with_chatgpt(cleaned_text,translated_text_google)
-            
-
-
-        st.subheader("谷歌翻译, Chatgpt润色:")    
-        st.write(edited_text)
-        st.write('---')
